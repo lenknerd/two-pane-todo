cc258237faf37c6e2808a8b026acb8a6d23b1e32
David Lenkner <lenknerd@gmail.com>
2013-09-22 16:49:41 -0400

    Initial format in index, function decs.

---
+++ todo.js
@@ -0,0 +1,22 @@
+// Retrieve database name from url, given naming convention in CouchDB
+var DATABASE = "/" + window.location.href.split("/")[3];
+
+// Get tasks from database
+function getTasks() {
+
+}
+
+// Show tasks
+function displayTasks(tasks) {
+
+}
+
+// Add task to (right-screen) todo list
+function addTask() {
+
+}
+
+// Create view into CouchDB dbase (will fail if already exists)
+function createView() {
+
+}
