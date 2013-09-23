5e81528ae71554dfc22a3babae972b926c656ce1
David Lenkner <lenknerd@gmail.com>
2013-09-22 20:13:39 -0400

    Added leftorright member to value

--- todo.js
+++ todo.js
@@ -2,21 +2,50 @@
 var DATABASE = "/" + window.location.href.split("/")[3];
 
 // Get tasks from database
-function getTasks() {
+function getAndShowTasks() {
 
 }
 
 // Show tasks
-function displayTasks(tasks) {
+function showTasks(tasks) {
 
 }
 
 // Add task to (right-screen) todo list
 function addTask() {
+    var desc = prompt("Enter a task description");
+    if (desc) {
+        var task = {
+            "task": desc,
+            "leftorright": "left"
+        };
 
+        $.ajax({
+            type: "POST",
+            url: DATABASE,
+            contentType: "application/json",
+            data: JSON.stringify(task),
+            success: function () {
+                getAndShowTasks();
+            }
+        });
+    }
 }
 
-// Create view into CouchDB dbase (will fail if already exists)
+// Create view into CouchDB dbase -- run at page load, fails if already exists
 function createView() {
-
+    var view = {
+       "language": "javascript",
+       "views": {
+           "tasks": {
+               "map": "function(doc) {if (doc.task) {emit(doc.task, doc);}}"
+           }
+       }
+    }
+    $.ajax({
+        type: "PUT",
+        url: DATABASE + "/_design/tasks",
+        contentType: "application/json",
+        data: JSON.stringify(view)
+     });
 }
